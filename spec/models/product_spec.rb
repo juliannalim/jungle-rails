9df3caee99196c86de568262f48eb62ee533b091
juliannalim <julianna.j.lim@gmail.com>
2020-09-21 14:53:36 -0400

    minor changes, removed a space

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
 
     it 'should save each field successfully' do
       category = Category.new(name: "books")
