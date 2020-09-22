affd71cdde6405e0dde315d400dc0865ca714b21
juliannalim <julianna.j.lim@gmail.com>
2020-09-22 13:34:37 -0400

    authentication for password and email with the test case, also fixed the last two validations for user regarding duplication email and email length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,15 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: {minimum: 5}
   validates :password_confirmation, presence: true
 
-  
+  def self.authenticate_with_credentials(email, password)
+    email_no_case = email.downcase.strip
+    user = User.find_by_email(email_no_case)
+    if user && user.authenticate(password)
+        user
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,10 +43,47 @@ RSpec.describe User, type: :model do
       test_user.save
       userOne = User.new(name: 'Itachi', email: 'sakura@gmail.com', password: 'password', password_confirmation: 'password')
       userOne.save
-      expect(test_user.errors.messages).to include("email already exist")
+      expect(userOne.errors.messages[:email]).to include("has already been taken")
+    end
+
+    it 'should produce error if password is < 5 characters' do
+      test_user = User.new(name: 'Sakura', email: 'sakura@gmail.com', password: 'pass', password_confirmation: 'pass')
+      test_user.save
+      expect(test_user.errors.messages[:password]).to include("is too short (minimum is 5 characters)")
     end
 
     describe '.authenticate_with_credentials'  do
+      it 'should let user login' do
+        test_user = User.new(name: 'Sakura', email: 'sakura@gmail.com', password: 'password', password_confirmation: 'password')
+        test_user.save
+        auth_user = User.authenticate_with_credentials('sakura@gmail.com', 'password')
+        #  puts "#{auth_user.inspect}"
+        expect(auth_user).to_not be_nil
+      end
+
+      it 'should not let user login with the wrong password' do
+        test_user = User.new(name: 'Sakura', email: 'sakura@gmail.com', password: 'password', password_confirmation: 'password')
+        test_user.save
+        auth_user = User.authenticate_with_credentials('sakura@gmail.com', 'notpassword')
+        #  puts "#{auth_user.inspect}"
+        expect(auth_user).to be_nil
+      end
+
+      it 'should let users to log in with wrong case in email' do
+        test_user = User.new(name: 'Sakura', email: 'sakura@gmail.com', password: 'password', password_confirmation: 'password')
+        test_user.save
+        auth_user = User.authenticate_with_credentials('Sakura@gmail.com', 'password')
+        #  puts "#{auth_user.inspect}"
+        expect(auth_user).to_not be_nil
+      end
+
+      it 'should let users to log in with white space in email' do
+        test_user = User.new(name: 'Sakura', email: 'sakura@gmail.com', password: 'password', password_confirmation: 'password')
+        test_user.save
+        auth_user = User.authenticate_with_credentials(' sakura@gmail.com ', 'password')
+        #  puts "#{auth_user.inspect}"
+        expect(auth_user).to_not be_nil
+      end
     end
   end
 end
